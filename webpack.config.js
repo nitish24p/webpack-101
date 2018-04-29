55800326719caf016c0a2eb24712dfb17b0efb21
NitishPhanse <nitsums@gmail.com>
2018-04-30 02:08:33 +0530

    added babel preset for react and root tag in html

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
     path: path.resolve(__dirname, 'public', 'dist'),
     filename: 'bundle.js',
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+            compact: false,
+            babelrc: false
+          }
+        },
+        exclude: /node_modules/
+      },
+    ]
+  }
 }

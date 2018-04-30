696060261e67fbe677888e8b751ba880b5da9cd8
NitishPhanse <nitsums@gmail.com>
2018-05-01 01:23:09 +0530

    removed dummy async await

--- src/Heading.js
+++ src/Heading.js
@@ -4,17 +4,5 @@ const Heading = (props: Object) => (
   <h1>{props.children}</h1>
 );
 
-async function f() {
-
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), 1000)
-  });
-
-  let result = await promise; // wait till the promise resolves (*)
-
-  alert(result); // "done!"
-}
-
-f();
 
 export default Heading;

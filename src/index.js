854dd8afd8b4c286e476c31a664a8037145b95e0
NitishPhanse <nitsums@gmail.com>
2018-05-03 02:27:31 +0530

    code spliting react router

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Switch, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
-import Heading from './components/Heading';
-import Banner from './components/Banner';
+import Loader from './components/Loader';
+import Home from './pages/Home';
+import asyncComponent from './asyncComponent';
+
+//  Common bundle
+// import About from './pages/About';
+// import Products from './pages/Products';
+// import Lectures from './pages/Lectures';
+// import Contact from './pages/Contact';
+
+const About = asyncComponent(() => import('./pages/About'), Loader);
+const Products = asyncComponent(() => import('./pages/Products'), Loader);
+const Lectures = asyncComponent(() => import('./pages/Lectures'), Loader);
+const Contact = asyncComponent(() => import('./pages/Contact'), Loader);
+
+import NavBar from './components/NavBar';
+
 import './styles/app.scss'
 
-import img from './images/webpack.png'
 
 const App = () => (
   <div>
-    <Banner imageSource={img} bannerText='Lets Learn Webpack'/>
-    <Heading>
-      The environment is {process.env.ENVIRONMENT}
-    </Heading>
-
-    <div className='header-container'>
-      <Heading>
-        This is a heading.
-      </Heading>
-      <Heading>
-        This is a another  heading.
-      </Heading>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/products" component={Products} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/lectures" component={Lectures} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </div>
 )
 
